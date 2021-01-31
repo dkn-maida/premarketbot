dc288fc0d9fad06908c02c5472f338b06580a6b6
maida <mehdi.aida.drakin@gmail.com>
2021-01-31 09:40:09 +0100

    Update premarketbot.py

--- premarketbot.py
+++ premarketbot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from ibapi.wrapper import *
 from ibapi.client import *
 from ibapi.contract import *
@@ -15,7 +14,7 @@ import math
 import argparse
 
 
-class TestWrapper(EWrapper):
+class Wrapper(EWrapper): 
 
     # error handling methods
     def init_error(self):
@@ -49,16 +48,30 @@ class TestWrapper(EWrapper):
         ## Overriden method
         self.my_time_queue.put(server_time)
 
+    #bar handling methods
+    def init_bar(self):
+        bar_queue = queue.Queue()
+        self.my_bar_queue=bar_queue
+        return bar_queue
+
+    def historicalData(self, reqId, bar):
+        ## Overriden method
+        self.my_bar_queue.put(bar)
+
+
+    
+
 
 # Below is the TestClient/EClient Class 
 
 '''Here we will call our own methods, not overriding the api methods'''
 
-class TestClient(EClient):
+class Client(EClient):
 
     def __init__(self, wrapper):
     ## Set up with a wrapper inside
         EClient.__init__(self, wrapper)
+        self.request_id = 0
 
     def server_clock(self):
         print("Asking server for Unix time")
@@ -78,17 +91,35 @@ class TestClient(EClient):
             print(self.get_error(timeout=5))
         return requested_time
 
+    def getOpenBar(self, contract):
+        print("Asking the first 5 min candle")
+        bar_storage = self.wrapper.init_bar()
+        end_date=datetime.datetime(year=now.year, month=now.month, day=now.day-1, hour=15, minute=35)
+        end_date=end_date.strftime("%Y%m%d %H:%M:%S")
+        self.reqHistoricalData(self.request_id, contract, end_date, "300 S", "5 mins", "TRADES", 1, 1, False, [])
+        self.request_id+=1
+        max_wait_time = 10
+        try:
+            requested_bar = bar_storage.get(timeout = max_wait_time)
+        except queue.Empty:
+            print("The queue was empty or max time reached")
+            requested_bar = None
+        while self.wrapper.is_error():
+            print("Error:")
+            print(self.get_error(timeout=5))
+        return requested_bar
 
-class TestApp(TestWrapper, TestClient):
+
+class Bot(Wrapper, Client):
     #Intializes our main classes 
     def __init__(self, ipaddress, portid, clientid):
-        TestWrapper.__init__(self)
-        TestClient.__init__(self, wrapper=self)
+        Wrapper.__init__(self)
+        Client.__init__(self, wrapper=self)
 
         #Connects to the server with the ipaddress, portid, and clientId specified in the program execution area
         self.connect(ipaddress, portid, clientid)
         self.nextorderId = None
-
+        
         #Initializes the threading
         thread = Thread(target = self.run)
         thread.start()
@@ -96,9 +127,6 @@ class TestApp(TestWrapper, TestClient):
 
         #Starts listening for errors 
         self.init_error()
-
-        
-
 
 def createContract(symbol: str) -> Contract:
     contract = Contract()
@@ -108,7 +136,7 @@ def createContract(symbol: str) -> Contract:
     contract.exchange = "SMART"
     return contract
 
-def createOrder(action, quantity, stop, target, crash):
+def createConditionalOrder(action, quantity, stop, target, crash) -> Order:
     # Fills out the order object 
     order1 = Order()    # Creates an order object from the import
     order1.action = action   # Sets the order action to buy
@@ -119,9 +147,12 @@ def createOrder(action, quantity, stop, target, crash):
 
 if __name__ == '__main__':
 
+    MAX_RISK = 100
+
     argp = argparse.ArgumentParser()
     argp.add_argument("symbol", nargs='+', help="stock list to run the bot on")
     argp.add_argument( "-H","--host", type=str, default="127.0.0.1", help="host adress to connect to")
+    argp.add_argument( "-p","--port", type=int, default="7497", help="port to connect to")
     args = argp.parse_args()
 
     contracts = []
@@ -132,12 +163,12 @@ if __name__ == '__main__':
 
     print("before start")
     # Specifies that we are on local host with port 7497 (paper trading port number)
-    app = TestApp(args.host, 7496, 0)     
+    app = Bot(args.host, args.port, 0)     
     # A printout to show the program began
     print("The program has begun")
 
     now = datetime.datetime.now()
-    openTime=datetime.datetime(year=now.year, month=now.month, day=now.day, hour=8, minute=22)
+    openTime=datetime.datetime(year=now.year, month=now.month, day=now.day, hour=15, minute=35)
     openTime=datetime.datetime.timestamp(openTime)
     #assigning the return from our clock method to a variable 
     requested_time = app.server_clock()
@@ -147,6 +178,16 @@ if __name__ == '__main__':
         requested_time = app.server_clock()
     #printing the return from the server
     print("STARTED--> Now passing orders..." )
+    #recuperer la valeur de la barre en 5 min
+    for c in contracts:
+        bar = app.getOpenBar(c)
+        print("Symbol=", c.symbol)
+        print("High=", bar.high)
+        print("Low=", bar.low)
+        print("Body=", bar.high-bar.low)
+        quantity= MAX_RISK // bar.open
+        #createConditionalOrder()
+        createConditionalOrder()
+        
     # Optional disconnect. If keeping an open connection to the input don't disconnet
     # app.disconnect()
-

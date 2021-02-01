2c4bf0c7bf89cca5918952dd4f43e7f381c08e5e
maida <mehdi.aida.drakin@gmail.com>
2021-02-01 22:53:00 +0100

    Update premarketbot.py

--- premarketbot.py
+++ premarketbot.py
@@ -55,18 +55,6 @@ class Wrapper(EWrapper):
         errormessage = "IB returns an error with %d errorcode %d that says %s" % (id, errorCode, errorString)
         self.my_errors_queue.put(errormessage)
     
-    #id handling methods
-    ###TODO###
-    # def init_id(self):
-    #     id_queue = queue.Queue()
-    #     self.my_id_queue = id_queue
-    #     return id_queue
-
-    # def nextValidId(self, orderId):
-    #     self.init_id()
-    #     self.my_id_queue.put(orderId)
-
-    # time handling methods
     def init_time(self):
         time_queue = queue.Queue()
         self.my_time_queue = time_queue
@@ -95,17 +83,6 @@ class Wrapper(EWrapper):
         ## Overriden method
         self.my_bar_queue.put(bar)
 
-    #order handling methods
-    # def init_order_status(self):
-    #     order_status_queue = queue.Queue()
-    #     self.my_order_status_queue=order_status_queue
-    #     return order_status_queue
-
-    # def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
-    #     orderStatus = OrderStatus(orderId, status, filled, remaining, parentId, whyHeld)
-    #     self.my_order_status_queue.put(orderStatus)
-
-
 # Below is the TestClient/EClient Class 
 
 '''Here we will call our own methods, not overriding the api methods'''
@@ -113,18 +90,14 @@ class Wrapper(EWrapper):
 class Client(EClient):
 
     def __init__(self, wrapper):
-    ## Set up with a wrapper inside
         EClient.__init__(self, wrapper)
         self.request_id = 0
 
     def server_clock(self):
         print("Asking server for Unix time")
-        # Creates a queue to store the time
         time_storage = self.wrapper.init_time()
-        # Sets up a request for unix time from the Eclient
         self.reqCurrentTime()
-        #Specifies a max wait time if there is no connection
-        max_wait_time = 10
+        max_wait_time = 5
         try:
             requested_time = time_storage.get(timeout = max_wait_time)
         except queue.Empty:
@@ -140,7 +113,7 @@ class Client(EClient):
         contract_storage = self.wrapper.init_contract()
         self.reqContractDetails(self.request_id, contract)
         self.request_id+=1
-        max_wait_time = 10
+        max_wait_time = 5
         try:
             contract_details= contract_storage.get(timeout = max_wait_time)
         except queue.Empty:
@@ -155,11 +128,11 @@ class Client(EClient):
     def getOpenBar(self, contract):
         print("Asking the first 10 min candle")
         bar_storage = self.wrapper.init_bar()
-        end_date=datetime.datetime(year=now.year, month=now.month, day=now.day-2, hour=15, minute=40)
+        end_date=datetime.datetime(year=now.year, month=now.month, day=now.day, hour=15, minute=40)
         end_date=end_date.strftime("%Y%m%d %H:%M:%S")
         self.reqHistoricalData(self.request_id, contract, end_date, "300 S", "10 mins", "TRADES", 1, 1, False, [])
         self.request_id+=1
-        max_wait_time = 10
+        max_wait_time = 5
         try:
             requested_bar = bar_storage.get(timeout = max_wait_time)
         except queue.Empty:
@@ -169,36 +142,6 @@ class Client(EClient):
             print("Error:")
             print(self.get_error(timeout=5))
         return requested_bar
-
-    # def fireOrder(self, contract, order):
-    #     print("firing an order")
-    #     order_status_storage=self.wrapper.init_order_status()
-    #     self.placeOrder(order.orderId, contract, order)
-    #     # max_wait_time=10
-    #     # try:
-    #     #     order_status = order_status_storage.get(timeout = max_wait_time)
-    #     # except queue.Empty:
-    #     #     print("The queue was empty or max time reached")
-    #     #     order_status = None
-    #     # while self.wrapper.is_error():
-    #     #     print("Error:")
-    #     #     print(self.get_error(timeout=5))
-    #     # return order_status
-
-    def getId(self):
-        print("requesting id")
-        id_storage = self.wrapper.init_id()
-        self.reqIds(-1)
-        max_wait_time = 10
-        try:
-            req_id = id_storage.get(timeout = max_wait_time)
-        except queue.Empty:
-            print("The queue was empty or max time reached")
-            req_id = None
-        while self.wrapper.is_error():
-            print("Error:")
-            print(self.get_error(timeout=5))
-        return req_id
 
 
 class Bot(Wrapper, Client):
@@ -259,12 +202,9 @@ def createConditionalOrder(contract, action, quantity, target, stop, start) -> O
     app.request_id += 1
     stopLoss.action = "SELL" if action == "BUY" else "BUY"
     stopLoss.orderType = "STP"
-    #Stop trigger price
     stopLoss.auxPrice = stop
     stopLoss.totalQuantity = quantity
     stopLoss.parentId = orderMain.orderId
-    #In this case, the low side order will be the last child being sent. Therefore, it needs to set this attribute to True 
-    #to activate all its predecessors
     stopLoss.transmit = True
 
     return [orderMain, takeProfit, stopLoss]
@@ -287,23 +227,13 @@ if __name__ == '__main__':
         print(contract.symbol)
     try:
         print("before start")
-        # Specifies that we are on local host with port 7497 (paper trading port number)
         app = Bot(args.host, args.port, 0)     
-        # A printout to show the program began
         print("The program has begun")
         print("Cancelling all orders...")
         app.reqGlobalCancel()
         print("All orders have been cancelled")
-        
-
-        # print("Get request id...")
-        # oid=app.getId()
-        # if oid is None:
-        #     oid=0
-        # print("id= ",oid)
-        ####TODO####
+    
         app.request_id=0
-
 
         now = datetime.datetime.now()
         openTime=datetime.datetime(year=now.year, month=now.month, day=now.day, hour=10, minute=00)
@@ -358,8 +288,7 @@ if __name__ == '__main__':
             print("Stop is=", stop)
             orders=createConditionalOrder(contract, "BUY", quantity, target, stop, bar.high)
             for order in orders:
-                status=app.fireOrder(contract, order)
-                print(status)
+                app.placeOrder(order.orderId, contract, order)
 
             move=round(max(body, rangeDown), 2)
             target=round((bar.low - move), 2)
@@ -371,20 +300,13 @@ if __name__ == '__main__':
             print("Stop is=", stop)
             orders=createConditionalOrder(contract, "SELL", quantity, target, stop, bar.low)
             for order in orders:
-                # status=app.fireOrder(contract, order)
-                # print(status)
                 app.placeOrder(order.orderId, contract, order)
 
     except Exception as err:
-        print("Shit happens ", type(err).__name__)
-        while app.is_error():
-            print("Error:")
-            print(app.get_error(timeout=5))
+        print("Shit happens: ", type(err).__name__)
+        print(err)
     finally:
         app.disconnect()
-    
-    # app.reqGlobalCancel()
-    # Optional disconnect. If keeping an open connection to the input don't disconnet
     
 
 
